469f0ab7ecf61fa14fe8b742d9a7568283b7db03
Carlos Alberto <carlos.alberto@travelperk.com>
2022-07-27 18:01:32 +0200

    mak email required

--- app/core/models.py
+++ app/core/models.py
@@ -9,6 +9,9 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("User should have an email")
+
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self.db)

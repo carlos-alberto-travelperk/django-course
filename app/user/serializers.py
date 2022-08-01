09817cfe9f72a622754f6f82216d216b9e583357
Carlos Alberto <carlos.alberto@travelperk.com>
2022-08-01 15:28:36 +0200

    update user

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -17,6 +17,16 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validate_data):
+        password = validate_data.pop("password", None)
+        user = super().update(instance, validate_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     email = serializers.EmailField()
